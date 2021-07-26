dc7ca1adba694f549ee9b565c96063c10f8357f3
Marco Di Summa <mdisumma@yahoo.com>
2021-07-26 11:24:39 +0100

    min

--- js/door.js
+++ js/door.js
@@ -1,10 +1,7 @@
-const click = document.querySelector(".click");
-const header = document.querySelector(".header");
-const title = document.querySelector("h1");
 const doorLeft = document.querySelector(".left");
 const doorRight = document.querySelector(".right");
 
-const soundToogle = document.querySelector(".audioSwich");
+// const soundToogle = document.querySelector(".audioSwich");
 const sound = document.querySelector("audio");
 
 window.addEventListener("load", (e) => {
@@ -15,20 +12,20 @@ window.addEventListener("load", (e) => {
 	}
 });
 
-const audioOff = document.querySelector(".audioOff");
-const audioOn = document.querySelector(".audioOn");
+// const audioOff = document.querySelector(".audioOff");
+// const audioOn = document.querySelector(".audioOn");
 
-audioOn.style.display = "block";
-audioOff.style.display = "none";
-soundToogle.addEventListener("click", (e) => {
-	if (audioOn.style.display === "block") {
-		audioOn.style.display = "none";
-		audioOff.style.display = "block";
-	} else {
-		audioOn.style.display = "block";
-		audioOff.style.display = "none";
-	}
-});
+// audioOn.style.display = "block";
+// audioOff.style.display = "none";
+// soundToogle.addEventListener("click", (e) => {
+// 	if (audioOn.style.display === "block") {
+// 		audioOn.style.display = "none";
+// 		audioOff.style.display = "block";
+// 	} else {
+// 		audioOn.style.display = "block";
+// 		audioOff.style.display = "none";
+// 	}
+// });
 
 function doorControl() {
 	doorLeft.classList.toggle("left");
@@ -36,7 +33,5 @@ function doorControl() {
 }
 
 function playSound() {
-	if (audioOn.style.display === "block") {
-		sound.play();
-	}
+	sound.play();
 }
